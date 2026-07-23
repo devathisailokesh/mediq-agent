d63f58c2fb71200ad18662ba3e33053ffe0cb89a
Devathi Sai Lokesh <devathi.sailokesh@chubb.com>
2026-07-23 23:52:26 +0530

    upgrade all packages to support python 3.14

--- _cleanup.py
+++
@@ -1,7 +0,0 @@
-import shutil, pathlib
-target = pathlib.Path(__file__).parent / "src" / "models"
-if target.exists():
-    shutil.rmtree(target)
-    print(f"Deleted: {target}")
-else:
-    print("Already gone")
